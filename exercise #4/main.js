67e511dfdd313e442f72809ff8354cf9346b95d9
abdinoo1 <apdirisaakahmed523@gmail.com>
2026-06-09 21:19:08 +0300

    add exercise #4

---
+++ exercise #4/main.js
@@ -0,0 +1,13 @@
+function add(x, b){
+    return x + b;
+}
+let  result = add(20, 20);
+console.log( result / 2);
+console.log(result);
+
+// function Expression
+const greet = function(x, b) {
+    console.log(x + b);
+};
+
+greet(10,20);
