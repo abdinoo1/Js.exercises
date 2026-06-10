0dc5a46f8f42dcf3551cee27232774b848fbd229
abdinoo1 <apdirisaakahmed523@gmail.com>
2026-06-10 14:38:40 +0300

    rename exercise folders

--- exercise #4/main.js
+++
@@ -1,4 +0,0 @@
-const add = (a, b) => a + b;
-
-console.log(add(8, 12)); // Output: 20
-console.log(add(14, 16)); // Output: 30
--- exercise-03/main.js
+++ exercise-03/main.js
@@ -2,4 +2,4 @@ let isSunny = true;
 let isWeekend= false;
 console.log(isSunny && !isWeekend);
 console.log(isSunny || isWeekend);
-console.log(!isSunny)
+console.log(!isSunny);
