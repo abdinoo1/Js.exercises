af66591d2f48684709ecea1b223840ff65dd8206
abdinoo1 <apdirisaakahmed523@gmail.com>
2026-07-05 12:46:58 +0300

    ku dar exericeskan

---
+++ exercise-12/main.js
@@ -0,0 +1,4 @@
+const fruits = ["tufaax", "moos","canbe","liin"];
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i])
+};
---
+++ exercise-14/main.js
@@ -0,0 +1,5 @@
+let number;
+do {
+    number = prompt("Enter a number greater than 15:");
+} while (number <= 15);
+console.log("mahadsanid!");
